a8272eee2aba717dec6f73319badd49a9a46c41c
vladblkvs <gremlinux@gmail.com>
2020-10-22 00:15:53 +0300

    feat: implement solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let result;
+    if (str.length % 2 !== 0) {
+        result = false;
+    }
+
+    const map = new Map(bracketsConfig);
+    let stack = [];
+    const arr = str.split('');
+    arr.forEach((el, i) => {
+        if (map.has(el) && !(el === stack[stack.length - 1] && el === map.get(el))) {
+            stack.push(el);
+        } else if (el === map.get(stack[stack.length - 1])) {
+            stack.pop();
+        }
+    });
+
+    if (stack.length === 0 && str.length % 2 === 0) {
+        result = true;
+    } else {
+        result = false;
+    }
+    console.log(result, stack);
+    return result;
 }
